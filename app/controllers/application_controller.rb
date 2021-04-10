3f1145ed288ee60c1849313e3f1b5ac096923e06
manchohin <a2138cm@aiit.ac.jp>
2021-04-10 15:06:29 +0000

    show goodbye text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   
   def hello
-    render html:"hello, world!"
+    #render html:"hello, world!"
+    render html:"hola, mundo!"
+  end
+  
+  def bye
+    render html:"goodbye, world!"
   end
 end
