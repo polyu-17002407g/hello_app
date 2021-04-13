212b52e2fe520694944322fbbfb3f2c7a1ea8898
manchohin <a2138cm@aiit.ac.jp>
2021-04-13 13:25:11 +0000

    hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   
   def hello
     #render html:"hello, world!"
-    render html:"hola, mundo!"
+    #render html:"hola, mundo!"
+    render html:"¡Hola, mundo!"
   end
   
   def bye
